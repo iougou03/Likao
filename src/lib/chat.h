075afc663c3ede6c6c74838577c637891b4a62ab
iougou03 <haesol820@naver.com>
2023-04-27 19:44:48 +0900

    공용 커스텀 라이브러리 chat.h 작성

---
+++ src/lib/chat.h
@@ -0,0 +1,17 @@
+// #include <openssl/sha.h>
+
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+#define DEFAULT_PORT 5500
+
+#define NAME_MAX_LEN 512
+#define PASSWORD_MAX_LEN 512
+
+struct user_t
+{
+    char name[NAME_MAX_LEN];
+    char password[SHA256_DIGEST_LENGTH * 2 + 1];
+};
+
+#endif
