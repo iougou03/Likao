b67264f88ef8f021de4f21510b10a7897739bb01
iougou03 <haesol820@naver.com>
2023-05-04 14:57:08 +0900

    서버 클라이언트간 소통 규약 설정

--- src/lib/chat.h
+++ src/lib/chat.h
@@ -11,7 +11,31 @@
 struct user_t
 {
     char name[NAME_MAX_LEN];
-    char password[SHA256_DIGEST_LENGTH * 2 + 1];
+    // char password[SHA256_DIGEST_LENGTH * 2 + 1]
+    char password[PASSWORD_MAX_LEN];
+};
+
+typedef const enum {
+    SIGN_IN,
+    SIGN_UP
+} msg_client_type;
+
+struct msg_from_client_t
+{
+    msg_client_type type;
+    char name[NAME_MAX_LEN];
+    char password[PASSWORD_MAX_LEN];
+};
+
+typedef const enum {
+    SUCCESS,
+    FAILED
+} msg_server_type;
+
+struct msg_from_server_t
+{
+    msg_server_type type;
+    char* msg;
 };
 
 #endif
