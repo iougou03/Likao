a0847889254c929b6ef5f29fd52d1af75ad2a4ee
iougou03 <haesol820@naver.com>
2023-05-09 19:06:48 +0900

    sign up 함수 구현 완료

--- src/server/util.h
+++ src/server/util.h
@@ -1,6 +1,19 @@
 #include <json-c/json.h>
 #include "../lib/chat.h"
 
+
 void json_to_strcut(struct json_object* j_obj, struct msg_from_client_t* msgp);
 
 void struct_to_json(struct json_object* j_obj, struct msg_from_server_t msg);
+
+
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+struct user_json_t {
+    char name[NAME_MAX_LEN];
+    char password[PASSWORD_MAX_LEN];
+    int created_at;
+};
+
+#endif
